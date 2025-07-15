489135736e1e96de32d1ae6c0c3f1ba59a8581d5
MrSorf <your_email>
2025-07-15 08:52:52 +0100

    update admin, with the ModelAdmin class

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
 from .models import Book
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "publication_year")   # add any options you like
+    search_fields = ("title", "author")
 
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
+
