6a75ae8facb80e798380bda5c7ea8d5673a7cf7f
MrSorf <your_email>
2025-07-15 08:55:38 +0100

    filter list added to admin file

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -4,6 +4,8 @@ from .models import Book
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publication_year")   # add any options you like
     search_fields = ("title", "author")
+    list_filter = ("author", "publication_year")   # choose any fields that make sense
+
 
 admin.site.register(Book, BookAdmin)
 
