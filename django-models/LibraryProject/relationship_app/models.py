e6af8a8f02b3c1b013292c08bd1a2a0370c89611
MrSorf <your_email>
2025-07-22 09:32:49 +0100

    method added to models

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=100)
+def __str__(self):
+    return self.name
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
@@ -11,7 +13,11 @@ class Book(models.Model):
 class Library(models.Model):
     name = models.CharField(max_length=100, default='Default Library')
     books = models.ManyToManyField('Book')
+def __str__(self):
+    return self.name
 
 class Librarian(models.Model):
     name = models.CharField(max_length=200)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+def __str__(self):
+    return self.name
