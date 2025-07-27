dd4cbc06fa7dfc772d5dea4a084090bfd7628e10
MrSorf <your_email>
2025-07-27 18:35:14 +0100

    corrected

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Library
+from .models import Book
 # Create your views here.
 def list_books(request):
-    return render(request, 'book_list.txt', content_type='text/plain')
+    books = Book.objects.all()
+    return render(request, 'book_list.txt', {'books': books}, content_type='text/plain')
 
 class BookListView(ListView):
     model = Library 
