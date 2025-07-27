18f0103e0af084857ac24e05db40a03727e38b8b
MrSorf <your_email>
2025-07-27 19:00:04 +0100

    opps

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from views import BookListView
+from views import list_books
+urlpatterns = [
+    path('books/', list_books, BookListView.as_view(), name='book_list'),
+]
