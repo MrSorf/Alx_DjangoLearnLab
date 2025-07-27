c54c6b9f5e2a99a63d864e674034b039ee744fc2
MrSorf <your_email>
2025-07-27 18:27:23 +0100

    files added

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Library
 # Create your views here.
+def list_books(request):
+    return render(request, 'book_list.txt', content_type='text/plain')
+
+class BookListView(ListView):
+    model = Library 
+    template_name = "book_list.txt"
+
+
