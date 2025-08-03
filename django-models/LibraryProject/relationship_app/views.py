a38b089171863dbeb54cb4f6095a658457c64dd7
MrSorf <your_email>
2025-08-03 09:25:31 +0100

    test

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,6 +8,10 @@ from django.contrib.auth import logout
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import FormView
 from django.urls import reverse_lazy
+from django.contrib.auth import admin
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.http import HttpResponse
+
 # Create your views here.
 def list_books(request):
     books = Book.objects.all()
@@ -37,3 +41,11 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, "/relationship_app/register.html", {"form": form})
+
+def is_admin(user):
+    return user.groups.filter(name='Admin').exists()
+
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return HttpResponse("Welcome, Admin!")
