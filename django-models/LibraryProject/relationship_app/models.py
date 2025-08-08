38f728e14edcc47b7fc8cf83788a8b9ccf28cd2f
MrSorf <your_email>
2025-08-08 07:33:43 +0100

    add

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -16,7 +16,8 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     
-    class Meta: [
+    class Meta: 
+        permissions = [
                ("can_add_book", "Can add a book"),
             ("can_change_book", "Can change a book"),
             ("can_delete_book", "Can delete a book"),
