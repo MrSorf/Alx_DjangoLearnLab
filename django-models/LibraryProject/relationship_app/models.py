1a29963bb40226586e64635da1e277c3e6040a4d
MrSorf <your_email>
2025-08-08 07:17:29 +0100

    kellog

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -27,7 +27,7 @@ class Librarian(models.Model):
 def __str__(self):
     return self.name
 
-class UserProfile(models.Model, User):  
+class UserProfile(models.Model):  
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
 
