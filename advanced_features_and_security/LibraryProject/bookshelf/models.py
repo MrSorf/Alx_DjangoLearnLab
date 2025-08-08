254ff587c61849749d298b16572be4bf65d6e3e6
MrSorf <your_email>
2025-08-08 08:58:24 +0100

    assdddd

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,32 +1,48 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 # Create your models here.
-class Book(models.Model):
-    title = models.CharField(max_length=200)
+# class Book(models.Model):
+#     title = models.CharField(max_length=200)
 
-    author = models.CharField(max_length=100)
+#     author = models.CharField(max_length=100)
 
-    publication_year = models.IntegerField()
+#     publication_year = models.IntegerField()
 
-    def __str__(self):
-        return f"{self.title} ({self.publication_year})"
-    class CustomUserManager(BaseUserManager):
+#     def __str__(self):
+#         return f"{self.title} ({self.publication_year})"
+    
+class CustomUserManager(BaseUserManager):
     def create_user(self, username, email=None, password=None, **extra_fields):
         if not email:
-            raise ValueError("Users must have an email address")
+              raise ValueError("Users must have an email address")
         email = self.normalize_email(email)
         user = self.model(username=username, email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
-
+    
     def create_superuser(self, username, email=None, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         return self.create_user(username, email, password, **extra_fields)
 
+
+    # class Article(models.Model):
+    #     title = models.CharField(max_length=255)
+    #     content = models.TextField()
+
+        
+       
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to="profiles/", null=True, blank=True)
 
     objects = CustomUserManager()
+
+    class Meta:
+            permissions = [
+                ("can_view", "Can view article"),
+                ("can_create", "Can create article"),
+                ("can_edit", "Can edit article"),
+                ("can_delete", "Can delete article"),
+            ]
