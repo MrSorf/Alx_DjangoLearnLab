8c4a93a0a15494eedeca8ad6e4706e6c723f60c8
MrSorf <your_email>
2025-08-08 07:48:55 +0100

    c

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -15,4 +15,7 @@ urlpatterns = [
     path('admin-page/', views.admin_view, name='admin_view'),
     path('librarian-page/', views.librarian_view, name='librarian_view'),
     path('member-page/', views.member_view, name='member_view'),
+     path('add_book/', views.add_book_view, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book_view, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book_view, name='delete_book'),
 ]
